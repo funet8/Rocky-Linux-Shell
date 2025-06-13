ca6a61f454bbbb01416aecf8e269817599739624
star <star@xgss.net>
2025-06-13 16:29:11 +0800

    自动更新文件

---
+++ update-repos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 添加远程仓库（如果尚未添加）
+git remote add gitee git@gitee.com:funet8/rocky-linux-shell.git
+git remote add github git@github.com:funet8/Rocky-Linux-Shell.git
+
+# 查看当前远程仓库
+git remote -v
+
+# 添加并提交更改
+git add .
+git commit -m "更新文件"
+
+# 推送至所有远程仓库
+git push --all
+
+echo "文件已成功更新到所有仓库"
